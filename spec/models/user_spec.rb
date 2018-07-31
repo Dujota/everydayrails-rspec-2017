a8ee3d80ab7995e357f2ac6f48c9b364d3213286
Denis <ddujota@internationalsafety.com>
2018-07-30 22:26:33 -0400

    filled in my first spec description

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a first name, last name,  email, and password"
+  it "is invalid without a first name"
+  it "is invalid without"
+  it "is invalid without"
+  it "is invalid without"
+  it "returns a user's full name as a string"
 end
