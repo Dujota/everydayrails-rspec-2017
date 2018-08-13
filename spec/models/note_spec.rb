7745a6331b989ba15cc00606903423e273b575c3
Denis <ddujota@internationalsafety.com>
2018-08-13 16:19:54 -0400

    filled in the before each hooks

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,13 +2,85 @@ require 'rails_helper'
 # belongs to user and project, must have a project and a user in order to be valid
 RSpec.describe Note, type: :model do
 
-  before do
-
+  before(:each) do
+    # Set up test data for all tests in file
   end
 
   ## Validation tests
+  describe 'Validates the Notes Model' do
+
+    before(:each) do
+      # set up test data for all tests in the file
+    end
+
+    context 'Create a valid object' do
+      it "is valid with User and a Message" do
+        user = User.create(
+          first_name: "Joe",
+          last_name: "Doe",
+          email: "test@example.com",
+          password: "secret"
+        )
+
+        project = user.projects.create(
+          name: "Test Project"
+        )
+
+        note1 = project.notes.create(
+          message: "This Note should be valid",
+          user: user
+        )
+
+        expect(note1).to be_valid
+      end
+    end
+
+    context "create invalid object" do
+      it "Is NOT valid without a User" do
+        user = User.create(
+          first_name: "Joe",
+          last_name: "Doe",
+          email: "test@example.com",
+          password: "secret"
+        )
+
+        project = user.projects.create(
+          name: "Test Project"
+        )
+
+        note1 = project.notes.create(
+          message: "This Note should be valid",
+        )
+
+        expect(note1).to_not be_valid
+      end
+
+      it "is NOT valid without a message" do
+        user = User.create(
+          first_name: "Joe",
+          last_name: "Doe",
+          email: "test@example.com",
+          password: "secret"
+        )
+
+        project = user.projects.create(
+          name: "Test Project"
+        )
+
+        note1 = project.notes.create(
+          user: user
+        )
+
+        expect(note1).to_not be_valid
+      end
+    end
+  end
 
   describe 'search message for a term' do
+    before do
+      # setup extra test data for all tests related to search
+    end
+
     context "when a match is found" do
       it 'returns notes that match the search term' do
         user = User.create(
